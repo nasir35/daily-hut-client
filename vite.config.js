2a18a398dd88c6076f14256724eadb7b8590c70b
nasir35 <nasir2242001@gmail.com>
2024-06-03 06:10:25 +0600

    installed necessary packages and created folder for file structures

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@pages": "/src/pages",
+      "@components": "/src/components",
+      "@images": "/src/assets/images",
+    },
+  },
+})
